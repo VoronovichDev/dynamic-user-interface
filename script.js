f81d2233960a457237110bec8186b329fb38cd37
Voronovich_dev <alexanderbizv1@gmail.com>
2022-08-30 22:59:45 +0300

    added logic for switching slides on button click

--- script.js
+++ script.js
@@ -6,6 +6,10 @@ const prevBtn = document.getElementById('prev-btn');
 
 const dots = document.getElementById('dots')
 
+const dot1 = document.getElementById('d1');
+const dot2 = document.getElementById('d2');
+const dot3 = document.getElementById('d3');
+
 let imgCounter = 0;
 
 // create images
@@ -24,3 +28,48 @@ function createSlide(img) {
 }
 
 createSlide('img/pic1.jpg')
+
+// clear frame before loading new img
+
+
+let currentClick = 0
+
+nextBtn.addEventListener('click', () => {
+   currentClick += 1
+   if (currentClick === 1 || currentClick === -2) {
+      clearFrame()
+      createSlide('img/pic2.jpg')
+   } else if (currentClick === 2 || currentClick === -1) {
+      clearFrame();
+      createSlide('img/pic3.jpg');
+   }
+   else {
+      clearFrame();
+      createSlide('img/pic1.jpg');
+      currentClick = 0
+   }
+})
+
+prevBtn.addEventListener('click', () => {
+   currentClick -= 1
+   clearFrame()
+   createSlide('img/pic1.jpg')
+   if (currentClick === 1 || currentClick === -2) {
+      clearFrame()
+      createSlide('img/pic2.jpg')
+   } else if (currentClick === 2 || currentClick === -1) {
+      clearFrame();
+      createSlide('img/pic3.jpg');
+   }
+   else {
+      clearFrame();
+      createSlide('img/pic1.jpg');
+      currentClick = 0
+   }
+})
+
+function clearFrame() {
+   slides.childElementCount != 0
+      ? slides.removeChild(slides.firstChild)
+      : null
+}
